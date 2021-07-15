2eaa6c43d86cd1e97deba9cde6a3ef85bd5c48eb
kehsihba19 <abhi12435@gmail.com>
2021-07-15 23:32:20 +0530

    [Fix] Added support for loading response

--- Frontend/apps/single.py
+++ Frontend/apps/single.py
@@ -8,11 +8,12 @@ def app():
     
     if st.button("Submit"):
         if(len(single_review)):
-            url='https://fastapi-review.herokuapp.com/api/'
-            x=requests.post(url,json={'review':single_review}).json()
-            if x['Message']=='Positive':
-                st.success("""## Great Work there! People liked your product 😃""")
-            else:
-                st.error("""## Try improving your product! People didn't find your product upto the mark 😔""")
+            with st.spinner('Analysing the product review'):
+                url='https://fastapi-review.herokuapp.com/api/'
+                x=requests.post(url,json={'review':single_review}).json()
+                if x['Message']=='Positive':
+                    st.success("""## Great Work there! People liked your product 😃""")
+                else:
+                    st.error("""## Try improving your product! People didn't find your product upto the mark 😔""")
         else:
             st.info("Enter the text to be reviewed")
--- Frontend/apps/thanks.py
+++ Frontend/apps/thanks.py
@@ -3,8 +3,7 @@ import requests
 
 def app():
     st.title('Thank you')
-
-    st.success('Here is a cute doggo to make you happy.')
-
-    x=requests.get('https://dog.ceo/api/breed/shiba/images/random').json()
-    st.image(x['message'],use_column_width='always' )
+    with(st.spinner("Calling a happy doggo for you")):
+        x=requests.get('https://dog.ceo/api/breed/shiba/images/random').json()
+        st.success('Here is a cute doggo to make you happy.')
+        st.image(x['message'],use_column_width='always' )
